0fc5b08f68d3c8e54ba1851d24e655ee9b303f7f
mmix4el <109658420+mmix4el@users.noreply.github.com>
2022-08-14 12:46:38 +0500

    Update config.h
    change  selbgcolor,  selbordercolor

--- config.h
+++ config.h
@@ -21,8 +21,8 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#ffae00";
+static char selbgcolor[]            = "#ffae00";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
